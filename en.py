2f0bebeef352da370f129182c945a479445bb1ee
Jack-19 <total90-@hotmail.com>
2021-10-27 18:09:56 +0000

    flask test

--- en.py
+++ en.py
@@ -2,6 +2,6 @@ import os
 
 os.environ.setdefault("IP", "0.0.0.0")
 os.environ.setdefault("PORT", "5000")
-os.environ.setdefault("SECRET_KEY", "your_secret_key_here")
-os.environ.setdefault("MONGO_URI", "mongodb+srv://<USERNAME>:<PASSWORD>@<CLUSTER>-4g3i1.mongodb.net/<DATABASE>?retryWrites=true&w=majority")
-os.environ.setdefault("MONGO_DBNAME", "task_manager")
+os.environ.setdefault("SECRET_KEY", "TSfCzqvuEy815T5LYE88KJxWgz2xcaDG")
+os.environ.setdefault("MONGO_URI", "mongodb+srv://JACK01:millwall21@breakfastlunchdinner.6mvme.mongodb.net/breakfastLunchDinner?retryWrites=true&w=majority")
+os.environ.setdefault("MONGO_DBNAME", "breakfastLunchDinner")
