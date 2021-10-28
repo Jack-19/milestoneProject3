9d015c45773600d471f538ef66a7320d41e97822
Jack-19 <total90-@hotmail.com>
2021-10-28 17:59:07 +0000

    mongo connection errror present

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 import os
-from flask import Flask
-if os.path.exists("env.py"):
-    import env
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+if os.path.exists("en.py"):
+    import en
+
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
 
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+@app.route("/get_tasks")
+def get_meals():
+    tasks = mongo.db.meal-type.find()
+    return render_template("index.html", tasks=tasks)
 
 
 
