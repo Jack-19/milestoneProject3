c4221058a8f61120e10434d810ef7d13a98ac5b4
Jack-19 <total90-@hotmail.com>
2021-10-29 20:51:10 +0000

    signup form created, link bug present for signup form

--- app.py
+++ app.py
@@ -19,14 +19,22 @@ mongo = PyMongo(app)
 @app.route("/")
 def get_breakfast():
     breakfast = list(mongo.db.breakfast.find({"type": "breakfast"}))
-    print(breakfast)
-    lunch = list(mongo.db.lunch.find({"type": "lunch"}))
-    print(lunch)
-    dinner = list(mongo.db.dinner.find({"type": "dinner"}))
-    print(dinner)
     return render_template("index.html", breakfast=breakfast)
     
+
+@app.route("/") 
+def get_lunch():
+    lunch = list(mongo.db.lunch.find({"type": "lunch"}))
+    return render_template("index.html", lunch=lunch)
+    
+
+@app.route("/")
+def get_dinner():
+    dinner = list(mongo.db.dinner.find({"type": "dinner"}))
+    return render_template("index.html", dinner=dinner)
+
 if __name__ == "__main":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
+
