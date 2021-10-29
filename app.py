ff07b8cef0658abf188b761f2cc799f505149ae3
Jack-19 <total90-@hotmail.com>
2021-10-29 18:04:06 +0000

    heroku and mongo connection corrected

--- app.py
+++ app.py
@@ -1,41 +1,28 @@
 import os
-from flask import (
-    Flask, flash, render_template,
-    redirect, request, session, url_for)
+from flask import Flask, render_template
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
-if os.path.exists("en.py"):
-    import en
+if os.path.exists("env.py"):
+    import env
 
 
 app = Flask(__name__)
+
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
+
 mongo = PyMongo(app)
 
-app = Flask(__name__,template_folder = 'templates')
 
 @app.route("/")
 def get_meals():
-    return render_template("index.html")
+    recipes = list(mongo.db.recipes.find({"meal-type": "dinner"}))
+    return render_template("index.html", recipes=recipes)
 
-
-get_meals()
-
-
-
-
-
-
-
-
-
-
-
-if __name__ == "__main__":
+    
+if __name__ == "__main":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
--- en.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ.setdefault("IP", "0.0.0.0")
-os.environ.setdefault("PORT", "5000")
-os.environ.setdefault("SECRET_KEY", "TSfCzqvuEy815T5LYE88KJxWgz2xcaDG")
-os.environ.setdefault("MONGO_URI", "mongodb+srv://JACK01:millwall21@breakfastlunchdinner.6mvme.mongodb.net/breakfastLunchDinner?retryWrites=true&w=majority")
-os.environ.setdefault("MONGO_DBNAME", "breakfastLunchDinner")
